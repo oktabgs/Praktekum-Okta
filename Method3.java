70ee6d0bbae1ee9a4972923aa487af0a5e4ffb5e
oktabgs <oktamab@gmail.com>
2017-04-05 20:47:05 +0700

    Materi Pertemuan ke 3  Oktama Bagaskara 14111194

---
+++ Method3.java
@@ -0,0 +1,10 @@
+public static void main(String[] args) {
+	MethodOverloading method = new MethodOverloading();
+	method.methodTampilkan("Belajar Java Yuks");
+	method.methodTampilkan("Belajar Java Yuks", "Hingga mengerti
+
+
+
+
+
+
